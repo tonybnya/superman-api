883a3d0c9a1bdaba919fa37ced8086eac79c4696
tonybnya <nya.tony2010@gmail.com>
2024-10-17 11:26:46 +0100

    perf: update the code to use model_dump() instead of the deprecated dict() method for Pydantic models

--- api/routers/products.py
+++ api/routers/products.py
@@ -15,6 +15,7 @@ router = APIRouter(
     tags=["products"]
 )
 
+
 # Pydantic models
 class ProductBase(BaseModel):
     """
@@ -48,7 +49,7 @@ async def create_product(product: ProductBase, db: Session = Depends(get_db)):
     """
     POST /products endpoint to create a product.
     """
-    db_product = ProductModel(**product.dict())
+    db_product = ProductModel(**product.model_dump())
     db.add(db_product)
     db.commit()
     db.refresh(db_product)
@@ -77,14 +78,18 @@ async def get_product(product_id: int, db: Session = Depends(get_db)):
 
 
 @router.put("/{product_id}", response_model=Product)
-async def update_product(product_id: int, updated_product: ProductBase, db: Session = Depends(get_db)):
+async def update_product(
+        product_id: int,
+        updated_product: ProductBase,
+        db: Session = Depends(get_db)
+):
     """
     PUT /products/{product_id} endpoint to update a product by its ID.
     """
     product = db.query(ProductModel).filter(ProductModel.id == product_id).first()
     if product is None:
         raise HTTPException(status_code=404, detail="Product not found")
-    for key, value in updated_product.dict().items():
+    for key, value in updated_product.model_dump().items():
         setattr(product, key, value)
     db.commit()
     db.refresh(product)
